c50ff669ee062635c7bc53ef5e9cec3cda424bf8
idaylu <idaylu@umich.edu>
2017-05-15 11:20:52 -0500

    Updated code in ruby file for vampires to adjust for allergies and the plot twist.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,29 @@
+def employee_info
+  counter= employee_number
+  until counter = 0
+    when age_verify && garlicbread && insurance = false
+      puts "Almost certainly a vampire."
+    when age_verify && (garlicbread || insurance) = true
+      puts "Probably not a vampire."
+    when age_verify && (garlicbread || insurance) = false
+      puts "Probably a vampire."
+    else
+      puts "Results inconclusive."
+      counter -= 1
+  end
+end
+
+def age_verify
+    if 2017-birthyear != age
+      return true
+    else false
+  end
+end
+
+
 #This is an employee survey to screen for vampires
+puts "How many employees need to be processed?"
+  employee_number=gets.chomp!.to_i
 puts "What is your name?"
   name=gets.chomp!
 puts "How old are you?"
@@ -7,68 +32,19 @@ puts "What year were you born?"
   birthyear=gets.chomp!.to_i
 puts "Our company cafeteria serves some amazing garlic bread. Should we order some for you?"
   garlicbread=gets.chomp!
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in company's health insurance?"
   insurance=gets.chomp!
+puts "What allergies do you have?"
+  allergies=gets.chomp!
 
-# if birthyear and age do not match then result false
-# if no to bread and insurance then result false
 
-
-#checks to see if age is equal to years from birth year
-def age_verify
-    if 2017-birthyear != age
-      return false
-    else true
-  end
-end
-
-#returns true if birth year and age match
-if age_verify = true
-  return true
-else false
-end
-
-#returns true if they accept garlic bread
-if garlicbread== "yes"
-  return true
-else
-  return false
-end
-
-if age_verify && (garlicbread || insurance) = true
-  puts "Probably not a vampire."
-end
-
-if age_verify && (garlicbread || insurance) = false
-  puts "Probably a vampire."
-end
-
-if age_verify && garlicbread && insurance = false
-  puts "Almost certainly a vampire."
-end
-
-def check_name
-  if name== "Drake Cula" or "Tu Fang"
+if name == "Drake Cula" or "Tu Fang"
     puts "Definitely a vampire."
-  end
-end
-
-if age_verify && garlicbread && insurance =true
-  then check_name
-end
-
-######
-
-when age_verify && garlicbread && insurance = false
-  puts "Almost certainly a vampire."
-when age_verify && (garlicbread || insurance) = true
-  puts "Probably not a vampire."
-when age_verify && (garlicbread || insurance) = false
-  puts "Probably a vampire."
+elsif allergies== "sunshine"
+    puts "Probably a vampire."
 else
-  puts "Results inconclusive."
+  puts employee_info
+end
 
-
-
-
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
