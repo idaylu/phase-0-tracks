73edff4be27f0ac65df16a8ead9d359270691758
idaylu <idaylu@umich.edu>
2017-05-19 11:10:07 -0500

    Added fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,3 +12,24 @@ end
 
 arr = [42, 89, 23, 1]
 p search_array(arr, 1)
+
+
+def fibona(length)
+  index=0
+  fibona_array= Array.new(length)
+
+  while index < fibona_array.length
+    if index <= 1
+      fibona_array[index]=index
+    else
+      fibona_array[index]= (fibona_array[index-2] + fibona_array[index-1])
+    end
+    index += 1
+  end
+  return fibona_array
+end
+
+p fibona(100)
+
+
+
