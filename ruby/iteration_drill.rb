48c99dc366dc09cbcdfda7468b2a87598e85d429
idaylu <idaylu@umich.edu>
2017-05-22 08:18:00 -0500

    Finished iteration drill

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -5,21 +5,61 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 
 # 1. Iterate through the zombie_apocalypse_supplies array,
 # printing each item in the array separated by an asterisk
-# ----
+
+zombie_apocalypse_supplies.each{|x| print x, '*'}
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
-# ----
+
+def organize(zombie_apocalypse_supplies)
+  count=zombie_apocalypse_supplies.length
+  loop do
+    switch_place=false
+    (count-1).times do |i|
+    if zombie_apocalypse_supplies[i] > zombie_apocalypse_supplies[i+1]
+    zombie_apocalypse_supplies[i], zombie_apocalypse_supplies[i+1]= zombie_apocalypse_supplies[i+1], zombie_apocalypse_supplies[i]
+        switch_place=true
+    end
+  end
+    break if not switch_place
+end
+  return zombie_apocalypse_supplies
+end
+
+puts organize(zombie_apocalypse_supplies)
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
-# ----
+
+puts "What item are you checking for?"
+item_input=gets.chomp.to_s
+stocked=false
+
+zombie_apocalypse_supplies.each do|item|
+  if items == item_input
+    stocked=true
+  end
+end
+
+if stocked==true
+  puts "Item exists."
+else
+  puts "Need more inventory of item."
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
-# ----
+
+new_pack=[]
+
+zombie_apocalypse_supplies.each do |item|
+  if new_pack.length <= 4
+    new_pack << item
+  end
+end
+
+p new_pack
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -28,7 +68,11 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # documentation for Arrays.
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
-# ----
+combined=other_survivor_supplies+zombie_apocalypse_supplies
+
+new_supply= combined.uniq
+
+puts new_supply
 
 # Hash Drills
 
@@ -44,17 +88,31 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+
+extinct_animals.each {|animal_name, year| print "#{{animal_name}- #{year} * "}
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
-# ----
+
+pre_millenium={}
+
+extinct_animals.each do |animal_name, year|
+  if year < 2000
+    pre_millenium[animal_name] = year
+  end
+end
+
+puts pre_millenium
+
 
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
-# ----
+
+extinct_animals.each do|animal_name,year|
+  extinct_animals[animal_name]= year - 3
+  end
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
@@ -62,10 +120,33 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Do not use any special built-in methods.
-# ----
+
+group_check=[
+  "Andean Cat",
+  "Dodo",
+  "Saiga Antelope"
+]
+
+is_extinct=false
+
+extinct_animals.each do |animal_name, year|
+  group_check.each do |animal_name_2,year_2|
+    if animal_name == animal_name_2
+      is_extinct=true
+    else
+      is_extinct=false
+    end
+
+    if is_extinct == true
+      puts "Animal is extinct."
+    end
+  end
+end
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
-# ----
+
+extinct_animals.delete_if { |animal_name,year| animal_name== "Passenger Pidgeon"  }
+
