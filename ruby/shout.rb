4410b9bae683f6c29255e36a6fc6ac400f5ded6d
idaylu <idaylu@umich.edu>
2017-05-30 06:09:44 -0500

    Added Mixin code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,35 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yell_happily(words)
-    words + "!!!" + " :)"
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# require_relative 'shout'
+# p Shout.yell_angrily("Oh Man")
+# p Shout.yell_happily("Oh Man")
+# p Shout.singleton_methods
+
+module Shout
+  def joy(words)
+    p "#{words}!! :)"
   end
 end
 
-require_relative 'shout'
-p Shout.yell_angrily("Oh Man")
-p Shout.yell_happily("Oh Man")
-p Shout.singleton_methods
+class Adult
+  include Shout
+end
+
+class Child
+  include Shout
+end
+
+#driver code
+adult=Adult.new
+adult.joy("Congrats")
+
+child=Child.new
+child.joy("Congrats")
