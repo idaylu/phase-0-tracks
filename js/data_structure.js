bce7ad874517ae0aae74c2e8c4a5a0ba83a09aca
idaylu <idaylu@umich.edu>
2017-06-01 11:06:29 -0500

    Added fuction for pairing colors and horses.

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,5 +1,15 @@
-var colors=["blue", "red","yellow","green"];
 var names=["Ed","Sea Biscuit","Black Beauty","Bob"];
+var colors=["blue", "red","yellow","green"];
 
 colors.push("purple");
 names.push("Buttercup");
+
+function pair(names,colors){
+var horse_color={};
+for(i=0;i<colors.length;i++){
+  horse_color[names[i]]=colors[i]
+}
+return horse_color;
+}
+
+console.log(pair(names,colors));
