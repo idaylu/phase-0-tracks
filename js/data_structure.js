fd76d7084233621ceee33c392616c3ea235409d2
idaylu <idaylu@umich.edu>
2017-06-01 11:57:10 -0500

    Added car creation constructor and made three cars.

--- js/data_structure.js
+++ js/data_structure.js
@@ -1,4 +1,5 @@
 var names=["Ed","Sea Biscuit","Black Beauty","Bob"];
+//Release 0 & 1
 var colors=["blue", "red","yellow","green"];
 
 colors.push("purple");
@@ -13,3 +14,29 @@ return horse_color;
 }
 
 console.log(pair(names,colors));
+
+//Release 2
+function Car(make,year,isRunning){
+  this.make=make;
+  this.year=year;
+  this.isRunning=isRunning;
+
+  this.rev=function(){console.log("Vroom!")};
+
+  console.log("CAR CREATION COMPLETE");
+};
+
+console.log("Lets build a new car!");
+var newCar1= new Car("Ford",2017,true);
+console.log(newCar1);
+console.log("-----");
+
+console.log("Lets build another new car!");
+var newCar2= new Car("Toyota",2005,true);
+console.log(newCar2);
+console.log("-----");
+
+console.log("Lets build one more new car!");
+var newCar2= new Car("Subaru",2016,true);
+console.log(newCar2);
+console.log("-----");
